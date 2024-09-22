c169192c4c23d64ff334babc89920df90d3bcd81
Sarah Smith <sarah@smithsoft.com.au>
2024-09-22 15:58:51 +1000

    Add game mode to launch player

---
+++ GunSurvivorsGame/Source/GunSurvivors/MainGameMode.h
@@ -0,0 +1,17 @@
+// (c) 2024 Sarah Smith
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MainGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GUNSURVIVORS_API AMainGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
