a1743a781f65e29969116eec05a68c1d1b442ce5
Sarah Smith <sez@storybridge.org>
2025-06-05 16:54:57 +1000

    Add own logging category

--- AdventureGame/Source/AdventureGame/AdventureGame.cpp
+++ AdventureGame/Source/AdventureGame/AdventureGame.cpp
@@ -3,4 +3,7 @@
 #include "AdventureGame.h"
 #include "Modules/ModuleManager.h"
 
+DEFINE_LOG_CATEGORY(LogAdventureGame);
+
 IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AdventureGame, "AdventureGame" );
+
